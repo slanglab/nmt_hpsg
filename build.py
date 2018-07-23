bafffd8bdd58ae2c0b97c0393bd64f68ddf53b7c
Johnny Wei <johntzwei@yahoo.com>
2018-07-22 20:37:33 -0400

    added splitting and raw data folder

--- build.py
+++ build.py
@@ -8,20 +8,23 @@ from sciluigi import TargetInfo
 import src.preprocess
 
 class Europarl(sciluigi.ExternalTask):
+    target = sciluigi.Parameter(default='raw/europarl/europarl-v7.fr-en.fr')
+    source = sciluigi.Parameter(default='raw/europarl/europarl-v7.fr-en.en')
+
     def out_europarl(self):
-        return [ TargetInfo(self, 'raw/europarl/europarl-v7.fr-en.fr'),
-                TargetInfo(self, 'raw/europarl/europarl-v7.fr-en.en') ]
+        return [ TargetInfo(self, self.target),
+                TargetInfo(self, self.source) ]
 
 
 class PreprocessEuroparl(sciluigi.Task):
     in_europarl = None
 
     def out_preproc(self):
-        return [ TargetInfo(self, 'data/europarl/europarl-v7.fr-en.fr'),
-                TargetInfo(self, 'data/europarl/europarl-v7.fr-en.en') ]
+        return [ TargetInfo(self, 'data/pre/preprocess/source'),
+                TargetInfo(self, 'data/pre/preprocess/target') ]
 
     def run(self):
-        call('mkdir data/europarl/', shell=True)
+        self.ex('mkdir -p data/pre/preprocess/')
         xml, blank = src.preprocess.preprocess_europarl(self.in_europarl[0].path,
                 self.in_europarl[1].path,
                 self.out_preproc()[0].path,
@@ -29,9 +32,32 @@ class PreprocessEuroparl(sciluigi.Task):
         logging.info('Removed %d lines with xml' % xml)
         logging.info('Removed %d blank lines' % blank)
 
-        call('wc -l raw/europarl/*', shell=True)
-        call('wc -l data/europarl/*', shell=True)
-        
+        logging.info('Line count of inputs:')
+        self.ex('wc -l raw/europarl/*', shell=True)
+        logging.info('Line count of preprocessed:')
+        self.ex('wc -l data/europarl/*', shell=True)
+
+
+class Split(sciluigi.Task):
+    splits = sciluigi.Parameter(default=10000)
+    digits = sciluigi.Parameter(default=4)
+
+    in_preproc = None 
+
+    def out_splits(self):
+        return [ TargetInfo(self, 'data/splits/%s' % ('%d' % i).zfill(self.digits)) \
+                for i in range(0, self.splits) ]
+
+    def run(self):
+        self.ex('mkdir -p data/splits/')
+        self.ex('split -l %d -d %d %s > data/splits/' % (self.splits, self.digits, self.in_preproc[1])) 
+
+class ParseWithLogon(sciluigi.Task):
+    in_splits = None
+
+    def out_parse(self):
+        pass
+
 
 class Run(sciluigi.WorkflowTask):
     def workflow(self):
@@ -39,8 +65,12 @@ class Run(sciluigi.WorkflowTask):
         preprocess = self.new_task('Preprocess Europarl', PreprocessEuroparl)
         preprocess.in_europarl = europarl.out_europarl()
 
-        return preprocess
+        split = self.new_task('Split Europarl', Split)
+        split.in_preproc = europarl.out_europarl()
+
+        return split
 
 
 if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
     luigi.run(main_task_cls=Run, local_scheduler=True)
